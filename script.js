6748ff1332404a60165fc2a06f630fbb735c90a2
Kotovar <77914431+Kotovar@users.noreply.github.com>
2023-08-25 00:24:22 +0700

    buttons 1-9, C, <- work

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 //переменные
-let firstNumber;
-let secondNumber;
-let operation;
+let firstNumber = "";
+let secondNumber = "";
+let operation = false;
+let input = document.getElementById("input");
+
 let buttons = document.getElementById("panel");
+
+const buttonValues = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+  zero: 0,
+};
 
 // <Функции базовых операторов + , - , * , /
 let addition = (a, b) => a + b;
@@ -29,4 +44,42 @@ buttons.addEventListener("mouseup", function (e) {
 
 buttons.addEventListener("mousedown", function (e) {
   e.target.style.backgroundColor = "rgb(139, 132, 132)";
+});
+
+//вызывается, когда нажата соответствующая кнопка с цифрой для передачи данных в переменную и обновления экрана
+function updateNumber(buttonValue) {
+  if (operation) {
+    firstNumber += buttonValue;
+    input.value = firstNumber;
+  } else {
+    secondNumber += buttonValue;
+    input.value = secondNumber;
+  }
+}
+
+//функция для ввода значений с калькулятора
+buttons.addEventListener("click", function (e) {
+  if (e.target.className in buttonValues) {
+    updateNumber(buttonValues[e.target.className]);
+    return;
+  }
+  switch (e.target.className) {
+    case "clear":
+      firstNumber = "";
+      secondNumber = "";
+      operation = false;
+      input.value = 0;
+      break;
+    case "backspace":
+      firstNumber = "";
+      secondNumber = "";
+      let test = input.value;
+      if (input.value.length == 1) {
+        input.value = 0;
+      } else {
+        input.value = test.slice(0, test.length - 1);
+        console.log(input.value);
+      }
+      break;
+  }
 });
